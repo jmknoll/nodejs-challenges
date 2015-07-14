0b142d55bbad84bdb3fcb3cc0d193661411fd14c
Jameson Knoll <j.knoll621@gmail.com>
2015-07-14 17:24:27 +0800

    modify answer

--- filter_ls/filter_ls.js
+++ filter_ls/filter_ls.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 var path = require('path');
 
-targetDir = process.argv[2];
-targetExt = process.argv[3];
-realTargetExt = '.' + targetExt;
+var targetDir = process.argv[2];
+var targetExt = process.argv[3];
+var realTargetExt = '.' + targetExt;
 
 
 fs.readdir(targetDir, function returnFiltered(err, list){
